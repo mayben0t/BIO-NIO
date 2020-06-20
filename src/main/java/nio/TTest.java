2b2561074e8b419f646d734dd545ef2dbba7c2bf
王骁 <594180536@qq.com>
2020-06-20 12:03:57 +0800

    nio基于单ByteBuffer实现双向读写

--- src/main/java/nio/TTest.java
+++ src/main/java/nio/TTest.java
@@ -2,6 +2,7 @@ package nio;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -14,11 +15,17 @@ public class TTest {
         FileInputStream fileInputStream = new FileInputStream(file);
         FileChannel inputChannel = fileInputStream.getChannel();
         inputChannel.read(byteBuffer);
-
+        fileInputStream.close();
 
         //再从ByteBuffer通过channel最终写到文件
         //翻转一次
         byteBuffer.flip();
+
+        File writeFile = new File("d:\\aaaaa\\fileCP.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(writeFile);
+        FileChannel outputChannel = fileOutputStream.getChannel();
+        outputChannel.write(byteBuffer);
+        fileOutputStream.close();
 
     }
 }
